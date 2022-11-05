774c80cd261c6ce84da8e2d64f59234e69f864e2
sunil1510k <sunil1510.k@gmail.com>
2022-11-05 11:32:01 +0530

    branch

---
+++ Files (1).py
@@ -0,0 +1,65 @@
+
+a = 10
+b = False
+c = 10
+d = "Hi"
+e = 10
+f = "Hi"
+g = False
+h = "hi"
+
+print('Memory address of a = ', id(a))
+print('Memory address of b = ', id(b))
+print('Memory address of c = ', id(c))
+print('Memory address of d = ', id(d))
+print('Memory address of e = ', id(e))
+print('Memory address of f = ', id(f))
+print('Memory address of g = ', id(g))
+print('Memory address of h = ', id(h))
+
+
+print( id(10) )
+print( id(False) )
+print( id("Hi") )
+
+
+
+
+func = enumerate("snnvce demdenbve")
+print(func)
+
+
+
+
+
+
+
+10
+50.3
+False
+5j
+"skcn vdcdcn c nb ewhb"
+
+rat
+cat
+bat
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
